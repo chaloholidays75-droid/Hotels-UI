21b36352163ee30dd6d5db4ff1ac7602fbc901ac
Chalo Holidays <chaloholidays75@gmail.com>
2025-08-26 12:32:04 +0530

    new changes

--- src/Hotel/HotelSalesList.jsx
+++ src/Hotel/HotelSalesList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./HotelSalesList.css";
+
 
 const HotelSalesList = () => {
   const [hotels, setHotels] = useState([]);
