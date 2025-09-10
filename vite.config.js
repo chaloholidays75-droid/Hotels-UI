502db5d28346917670335f5ac3f7b1d292f6d03e
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-10 14:49:22 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+    base: '/',
   plugins: [
     react(),
     tailwindcss(),
