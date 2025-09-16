662537cdd47c4c80d5f1f10977ccef09c5545ba7
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-16 17:42:35 +0530

    new changes

--- src/Agent/AgencyDetailsForm.jsx
+++ src/Agent/AgencyDetailsForm.jsx
@@ -3,7 +3,9 @@ import LocationSelector from '../components/LocationSelector';
 
 const AgencyDetailsForm = ({ formData, setFormData, errors, setErrors, handleChange, setCurrentPage }) => {
   const [showNotification, setShowNotification] = React.useState('');
-
+  console.log("Rendering AgencyDetailsForm");
+  console.log(LocationSelector === undefined ? "LocationSelector is undefined" : "LocationSelector is defined");
+  
   const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
