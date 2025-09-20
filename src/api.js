6139c1c6a7734d3ec23b1d6372147947a2cd98a1
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-20 15:10:12 +0530

    new changes

--- src/api.js
+++ src/api.js
@@ -204,7 +204,7 @@ export async function getHotelsByCity(cityId) {
 
 export const getRecentActivities = async () => {
   try {
-    const response = await axios.get(`${API_BASE}/dashboard/recent-activities`);
+    const response = await axios.get(`${API_BASE}/api/recent-activities`);
     // If your API wraps data in 'data' property
     return Array.isArray(response.data) ? response.data : response.data.data || [];
   } catch (error) {
