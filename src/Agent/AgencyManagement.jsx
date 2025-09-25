18a5a5a7b704022a95e6beab3be7a9d5978ba8c3
ChaloHolidays <chaloholidays75@gmail.com>
2025-09-25 14:47:39 +0530

    new changes

--- src/Agent/AgencyManagement.jsx
+++ src/Agent/AgencyManagement.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useContext } from 'react';
 import AgencyForm from './AgencyForm';
 import AgencyList from './AgencyList';
 import AgencyViewModal from './AgencyViewModal';
